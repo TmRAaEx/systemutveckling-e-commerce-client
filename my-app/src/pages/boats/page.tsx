c49e35fce24a254826705ab682182a093fc58fc1
Alexander-Hirsch <04alexander.hirsch@gmail.com>
2025-04-30 09:46:15 +0200

    added pages so the topbar works

---
+++ my-app/src/pages/boats/page.tsx
@@ -0,0 +1,5 @@
+import Text from "@components/UI/styled/Text.tsx";
+
+export default function BoatPage() {
+    return <Text>under development</Text>
+}
---
+++ my-app/src/pages/partners/page.tsx
@@ -0,0 +1,6 @@
+import Text from "@components/UI/styled/Text.tsx";
+
+export default function PartnersPage() {
+    return <Text>under development</Text>
+    
+}
