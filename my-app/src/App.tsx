b741cc68b1019103eb43d08ca85111ac3d064c66
Alexander-Hirsch <04alexander.hirsch@gmail.com>
2025-05-05 09:26:16 +0200

    Added cart provider

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,12 +1,15 @@
 import {RouterProvider} from "react-router"
 import {fileRouter} from "@tmraaex/simpleframework" //my own "framework" currently only contains router
 import {HelmetProvider} from "react-helmet-async";
+import {CartProvider} from "@providers/CartProvider.tsx";
 
 export default function App() {
     return (
         <div className="App">
             <HelmetProvider>
-                <RouterProvider router={fileRouter}/>
+                <CartProvider>
+                    <RouterProvider router={fileRouter}/>
+                </CartProvider>
             </HelmetProvider>
         </div>
     );
